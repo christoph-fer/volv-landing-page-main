5f53201a1df394d5b5d61b77900859746a4b36c1
christoph-fer <christopher.vfernandes@gmail.com>
2021-02-24 20:17:17 +0530

    Streamlined a working copy to clipboard JQuery

---
+++ v6.0/js/share-experience.js
@@ -0,0 +1,28 @@
+const shareButton = document.querySelector(".share-button");
+shareButton.addEventListener("click", (event) => {
+  if (navigator.share) {
+    navigator
+      .share({
+        title: document.title,
+        text: "Work at Volv",
+        url: $(location).attr("href"),
+      })
+      .then(() => {
+        console.log("Thanks for sharing!");
+      })
+      .catch(console.error);
+  } else {
+    var $temp = $("<input>");
+    var $url = $(location).attr('href');
+
+    $("body").append($temp);
+    $temp.val($url).select();
+    document.execCommand("copy");
+    $temp.remove();
+
+    $(".volv-browser-notif").addClass("active"),
+      setTimeout(function () {
+        $(".volv-browser-notif").removeClass("active");
+      }, 2000);
+  }
+});
